176351078f7d8f5749a025b7e7d138b722455289
annisa-rachma <saa.annisa15@gmail.com>
2025-05-26 20:48:58 +0700

    add demo pages and navigation

---
+++ src/app/components/navbar/Navbar.tsx
@@ -0,0 +1,45 @@
+'use client'
+
+import React from "react";
+import styles from "./navbar.module.css";
+import { useRouter } from "next/navigation";
+
+interface DemoProps {
+  title: string;
+  slug: string;
+}
+
+interface NavbarProps {
+  data: DemoProps[];
+}
+
+export default function Navbar({ data }: NavbarProps) {
+    const router = useRouter()
+
+    const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const slug = e.target.value
+        if(slug) {
+            router.push(`/demos/${slug}`)
+        }
+    }
+
+  return (
+    <div className={styles.navbar}>
+      <label htmlFor="demos"></label>
+      <select name="" id="demos" onChange={handleChange}>
+        <option value="" disabled selected>
+          -- Select a demo --
+        </option>
+        {data.map((demo, idx) => {
+          return (
+            <>
+              <option key={idx} value={demo.slug}>
+                {demo.title}
+              </option>
+            </>
+          );
+        })}
+      </select>
+    </div>
+  );
+}
---
+++ src/app/demos/project-mouse-hover/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ProjectMouseHover() {
+  return (
+    <div>test</div>
+  )
+}

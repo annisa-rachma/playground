dea72c87fb86e6f1086aa973b42e6409775c3096
annisa-rachma <saa.annisa15@gmail.com>
2025-05-28 13:51:14 +0700

    add animation

--- src/app/demos/project-mouse-hover/components/modal/Modal.tsx
+++ src/app/demos/project-mouse-hover/components/modal/Modal.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import styles from "./style.module.css";
 import Image from "next/image";
+import {motion, scale} from "framer-motion"
 
 interface projectData {
   title: string;
@@ -14,10 +15,16 @@ interface ModalProps {
   data: projectData[];
 }
 
+const scaleAnimation = {
+    initial: {scale: 0, x:"-50%", y:"-50%"},
+    open: {scale: 1, x:"-50%", y:"-50%", transition:{duration: 0.4, ease: [0.76, 0, 0.24, 1]}},
+    closed: {scale: 0, x:"-50%", y:"-50%", transition:{duration: 0.4, ease: [0.32, 0, 0.67, 0]}}
+}
+
 export default function Modal({ modal, data }: ModalProps) {
   const { active, index } = modal;
   return (
-    <div className={styles.container}>
+    <motion.div variants={scaleAnimation} initial={"initial"} animate={active? "open": "closed"} className={styles.container}>
       <div style={{ top: index * -100 + "%" }} className={styles.modalSlider}>
         {data.map((el, index) => {
           const { src, color } = el;
@@ -39,6 +46,6 @@ export default function Modal({ modal, data }: ModalProps) {
           );
         })}
       </div>
-    </div>
+    </motion.div>
   );
 }

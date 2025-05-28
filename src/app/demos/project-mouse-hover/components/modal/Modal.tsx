fe8d929d461b5973b0846e87d1977c1d43e31e4f
annisa-rachma <saa.annisa15@gmail.com>
2025-05-28 14:23:25 +0700

    add effect for in and out mouse to the project list

--- src/app/demos/project-mouse-hover/components/modal/Modal.tsx
+++ src/app/demos/project-mouse-hover/components/modal/Modal.tsx
@@ -1,8 +1,9 @@
 // "use client";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import styles from "./style.module.css";
 import Image from "next/image";
-import {motion, scale} from "framer-motion"
+import { motion } from "framer-motion";
+import gsap from "gsap";
 
 interface projectData {
   title: string;
@@ -16,15 +17,56 @@ interface ModalProps {
 }
 
 const scaleAnimation = {
-    initial: {scale: 0, x:"-50%", y:"-50%"},
-    open: {scale: 1, x:"-50%", y:"-50%", transition:{duration: 0.4, ease: [0.76, 0, 0.24, 1]}},
-    closed: {scale: 0, x:"-50%", y:"-50%", transition:{duration: 0.4, ease: [0.32, 0, 0.67, 0]}}
-}
+  initial: { scale: 0, x: "-50%", y: "-50%" },
+  open: {
+    scale: 1,
+    x: "-50%",
+    y: "-50%",
+    transition: { duration: 0.4, ease: [0.76, 0, 0.24, 1] },
+  },
+  closed: {
+    scale: 0,
+    x: "-50%",
+    y: "-50%",
+    transition: { duration: 0.4, ease: [0.32, 0, 0.67, 0] },
+  },
+};
 
 export default function Modal({ modal, data }: ModalProps) {
   const { active, index } = modal;
+  const container = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const moveContainerX = gsap.quickTo(container.current, "left", {
+      duration: 0.8,
+      ease: "power3",
+    });
+    const moveContainerY = gsap.quickTo(container.current, "top", {
+      duration: 0.8,
+      ease: "power3",
+    });
+
+    const handleMouseMove = (e:MouseEvent) => {
+        const { clientX, clientY } = e;
+        moveContainerX(clientX);
+        moveContainerY(clientY);
+    }
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+        window.removeEventListener("mousemove", handleMouseMove)
+    }
+  }, []);
+
   return (
-    <motion.div variants={scaleAnimation} initial={"initial"} animate={active? "open": "closed"} className={styles.container}>
+    <motion.div
+      ref={container}
+      variants={scaleAnimation}
+      initial={"initial"}
+      animate={active ? "open" : "closed"}
+      className={styles.container}
+    >
       <div style={{ top: index * -100 + "%" }} className={styles.modalSlider}>
         {data.map((el, index) => {
           const { src, color } = el;
